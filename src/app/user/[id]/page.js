760b132b325a3bac6bcc6b9b9a9653509833e213
Jan Alex Kazaku <Alex950218@seznam.cz>
2025-03-28 22:41:19 +0100

    routes functionality added, (next/navigation)

---
+++ src/app/user/[id]/page.js
@@ -0,0 +1,16 @@
+"use client"
+
+import React from "react";
+import { useRouter } from "next/navigation"
+
+export default function User({params}) {
+  const { id } = React.use(params);
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>User : {id}</h1>
+       <button onClick={router.back}>Go Back</button>
+    </div>
+  )
+}
