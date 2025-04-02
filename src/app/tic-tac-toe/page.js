62bf52399de31e9cb8e06cd8331e37010782f14b
Jan Alex Kazaku <Alex950218@seznam.cz>
2025-04-02 23:32:57 +0200

    finished ticTacToe game

--- src/app/tic-tac-toe/page.js
+++ src/app/tic-tac-toe/page.js
@@ -14,14 +14,17 @@ function Square({value, onPress}) {
   )
 }
 
-function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  
+function Board({squares, xIsNext, onPlay}) {
+
   function onSquareClick(idx) {
-    const newSquares = [...squares]
-    newSquares[idx] = "X"
-    setSquares(newSquares)
+    if(squares[idx] || calculateWinner(squares)) return;
+
+    const nextSquares = [...squares];
+    nextSquares[idx] = xIsNext ? "X" : "O";
+    onPlay(nextSquares);
   }
+
+  const winner = calculateWinner(squares)
 
   return (
     <>
@@ -40,19 +43,83 @@ function Board() {
         <Square value={squares[7]} onPress={()=> onSquareClick(7)}/>
         <Square value={squares[8]} onPress={()=> onSquareClick(8)}/>
       </div>
+      <div>
+        {winner ? (
+          <p> Winner is: {winner}</p>
+        ) : (
+          <p> Next Player: {xIsNext ? "X" : "O"} </p>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default function Game() {
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
 
+  for (let i = 0; i < lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      console.log(squares[a])
+      console.log(squares[b])
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move # " + move;
+    } else {
+      description = "Go to game"
+    }
+    return (
+      <li key={move + Math.random()}>
+        <button onClick={()=>jumpTo(move)}>{description}</button>  
+      </li>
+    );
+  })
+  
   return (
     <div>
       <div className="flex items-center justify-center bg-gray-500 p-2"  >
         <h1 className="text-white  ">Tic Tac Toe</h1>
       </div>
-      <div className="board">
-        <Board/>
+      <div className="board-wrapper">
+        <div className="board">
+          <Board squares={currentSquares} xIsNext={xIsNext} onPlay={handlePlay}/>
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
       </div>
     </div>
   )
