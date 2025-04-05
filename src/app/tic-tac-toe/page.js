ccd29e1fcc44420e8a7051e6dbf99952b171f9b0
Jan Alex Kazaku <107789732+Allex-code@users.noreply.github.com>
2025-04-05 21:58:29 +0200

    Array(25) in history bug instead of Array(9)

--- src/app/tic-tac-toe/page.js
+++ src/app/tic-tac-toe/page.js
@@ -106,7 +106,7 @@ function calculateWinner(squares) {
 
 
 export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [history, setHistory] = useState([Array(25).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
